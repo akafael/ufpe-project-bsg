9e8e764a2ccd7a278dab1b63df9f06e10c9bee1e
Rafael <akafael@users.noreply.github.com>
2023-09-02 20:38:54 -0300

    Add selectBSGMode function

--- code/bsg.h
+++ code/bsg.h
@@ -6,6 +6,39 @@
 
 #include <stdint.h>
 
+typedef enum {
+  BSG_INDLE = 0,
+  BSG_STARTER = 1,
+  BSG_MOTOR = 2,
+  BSG_GENERATOR = 3
+} StateBSG;
+
+typedef struct {
+  uint16_t velocity;
+  uint16_t angleAccPedal;
+  uint16_t angleBrakePedal;
+  uint8_t requestCarStart;
+} Vehicle;
+
+typedef struct {
+  StateBSG currentMode;
+  uint16_t rpm;
+  float voltage;
+  float current;
+} Engine;
+
+typedef struct {
+  uint16_t rpm;
+} BSG;
+
+typedef struct {
+  float voltage;
+  float current;
+} Battery;
+
+StateBSG selectBSGMode(Vehicle vehicle, Engine engine, BSG bsg,
+                       Battery battery);
+
 uint16_t sum(uint16_t a, uint16_t b);
 
 #endif // BSG_H
