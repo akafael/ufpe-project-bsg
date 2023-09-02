54ba824ce2d2ed985ab2cf552bf2e8ec5f01999a
Rafael <akafael@users.noreply.github.com>
2023-09-02 11:55:11 -0300

    Add basic test using assert for evaluating build

--- code/bsg.c
+++ code/bsg.c
@@ -1,9 +1,4 @@
 #include "bsg.h"
 #include <stdio.h>
 
-int main() {
-
-  printf("Hello, BSG!\n");
-
-  return 0;
-}
+uint16_t sum(uint16_t a, uint16_t b) { return a + b; }
---
+++ code/main.c
@@ -0,0 +1,11 @@
+#include "bsg.h"
+#include <stdio.h>
+
+int main() {
+
+  printf("Hello, BSG!\n");
+
+  printf("%d + %d = %d\n", 2, 3, sum(2, 3));
+
+  return 0;
+}
