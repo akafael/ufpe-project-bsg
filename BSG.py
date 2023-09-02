d0298e2a0d202df8f0d0f2e12ed488c7c9c05567
pjmbs <pedrobichara@gmail.com>
2023-09-02 11:52:11 -0300

    Introduction of BSG Python code

---
+++ BSG.py
@@ -0,0 +1,8 @@
+'''Prototype of Python code describing the BSG interactions
+
+    -Daniel Sarmento 
+    -Lucas Emanuel 
+    -Paulo Dantas
+    -Pedro Bichara
+    -Rafael Lima
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-'''
