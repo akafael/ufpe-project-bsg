198f11edf5a5abbe7f65906cf09427f35612a1b4
Paulo <p.dantas@outlook.com>
2023-09-05 14:50:18 +0000

    Added starter test case

--- code/bsg.c
+++ code/bsg.c
@@ -2,23 +2,23 @@
 #include "bsg.h"
 
 StateBSG selectBSGMode(Vehicle vehicle, Engine engine, BSG bsg, Battery battery) {
-  if (vehicle.angleAccPedal>70) {
-    return BSG_STARTER;
+   if (vehicle.requestCarStart != 0) {
+      return BSG_STARTER;
   };
   if (vehicle.velocity == 0) {
-      return BSG_IDLE;
-  };
-  if (vehicle.requestCarStart != 0) {
-      return BSG_STARTER;
+    return BSG_IDLE;
   };
   if (vehicle.angleBrakePedal > 0) {
       return BSG_GENERATOR;
   };
-  if (battery.voltage < 30) {
-      return BSG_IDLE;
+  if (vehicle.angleBrakePedal == 0){
+    return BSG_STARTER;
+  }
+  if (battery.voltage >= 0) {
+      return BSG_STARTER;
   };
-  if (battery.current > 100) {
-      return BSG_IDLE;
+  if (battery.current >= 0) {
+      return BSG_STARTER;
   };
-  return BSG_IDLE;
+  return BSG_STARTER;
 }
