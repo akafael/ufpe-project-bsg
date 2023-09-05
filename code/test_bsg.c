6e951c3b9788b9dd428f749df930ff102abcd353
Rafael <akafael@users.noreply.github.com>
2023-09-05 03:15:47 -0300

    Change struct initialization to designated initializer format

--- code/test_bsg.c
+++ code/test_bsg.c
@@ -7,24 +7,28 @@ UTEST_MAIN();
  * BSG IDLE whenever the engine is off and the vehicle stopped
  */
 UTEST(bsg, idle) {
-  Vehicle vehicle;
-  vehicle.velocity = 0;
-  vehicle.angleAccPedal = 0;
-  vehicle.angleBrakePedal = 0;
-  vehicle.requestCarStart = 0;
-
-  BSG bsg;
-  bsg.currentMode = BSG_INDLE;
-  bsg.rpm = 0;
-  bsg.voltage = 0;
-  bsg.current = 0;
-
-  Engine engine;
-  engine.rpm = 0;
-
-  Battery battery;
-  battery.voltage = 0;
-  battery.current = 0;
+  Vehicle vehicle = {
+    .velocity = 0,
+    .angleAccPedal = 0,
+    .angleBrakePedal = 0,
+    .requestCarStart = 0
+  };
+  
+  BSG bsg = {
+    .currentMode = BSG_INDLE,
+    .rpm = 0,
+    .voltage = 0,
+    .current = 0
+  };
+  
+  Engine engine = {
+    .rpm = 0
+  };
+  
+  Battery battery = {
+    .voltage = 0,
+    .current = 0
+  };
 
   const StateBSG bsgMode = selectBSGMode(vehicle, engine, bsg, battery);
   ASSERT_EQ(bsgMode, BSG_INDLE);
@@ -34,72 +38,84 @@ UTEST(bsg, idle) {
  * BSG IDLE whenever the engine is off and the vehicle stopped
  */
 UTEST(bsg, engineOff) {
-  Vehicle vehicle;
-  vehicle.velocity = 0;
-  vehicle.angleAccPedal = 0;
-  vehicle.angleBrakePedal = 0;
-  vehicle.requestCarStart = 0;
-
-  BSG bsg;
-  bsg.currentMode = BSG_INDLE;
-  bsg.rpm = 0;
-  bsg.voltage = 0;
-  bsg.current = 0;
-
-  Engine engine;
-  engine.rpm = 0;
-
-  Battery battery;
-  battery.voltage = 12;
-  battery.current = 1;
+  Vehicle vehicle = {
+    .velocity = 0,
+    .angleAccPedal = 0,
+    .angleBrakePedal = 0,
+    .requestCarStart = 0
+  };
+  
+  BSG bsg = {
+    .currentMode = BSG_INDLE,
+    .rpm = 0,
+    .voltage = 0,
+    .current = 0
+  };
+  
+  Engine engine = {
+    .rpm = 0
+  };
+  
+  Battery battery = {
+    .voltage = 12,
+    .current = 1
+  };
 
   const StateBSG bsgMode = selectBSGMode(vehicle, engine, bsg, battery);
   ASSERT_EQ(bsgMode, BSG_INDLE);
 }
 
 UTEST(bsg, startEngine) {
-  Vehicle vehicle;
-  vehicle.velocity = 0;
-  vehicle.angleAccPedal = 0;
-  vehicle.angleBrakePedal = 0;
-  vehicle.requestCarStart = 1;
-
-  BSG bsg;
-  bsg.currentMode = BSG_INDLE;
-  bsg.rpm = 0;
-  bsg.voltage = 0;
-  bsg.current = 0;
-
-  Engine engine;
-  engine.rpm = 0;
-
-  Battery battery;
-  battery.voltage = 12;
-  battery.current = 1;
-
+  Vehicle vehicle = {
+    .velocity = 0,
+    .angleAccPedal = 0,
+    .angleBrakePedal = 0,
+    .requestCarStart = 1
+  };
+  
+  BSG bsg = {
+    .currentMode = BSG_INDLE,
+    .rpm = 0,
+    .voltage = 0,
+    .current = 0
+  };
+  
+  Engine engine = {
+    .rpm = 0
+  };
+  
+  Battery battery = {
+    .voltage = 12,
+    .current = 1
+  };
+ 
   const StateBSG bsgMode = selectBSGMode(vehicle, engine, bsg, battery);
   ASSERT_EQ(bsgMode, BSG_STARTER);
 }
 
 UTEST(bsg, regenerativeBreak) {
-  Vehicle vehicle;
-  vehicle.velocity = 60;
-  vehicle.angleAccPedal = 0;
-  vehicle.angleBrakePedal = 50;
-  vehicle.requestCarStart = 0;
-
-  BSG bsg;
-  bsg.currentMode = BSG_INDLE;
-  bsg.rpm = 4000;
-  bsg.voltage = 48;
-  bsg.current = 0;
-
-  Engine engine;
-  engine.rpm = 4000;
-
-  Battery battery;
-  battery.voltage = 30;
-  battery.current = 1;
+  Vehicle vehicle = {
+    .velocity = 60,
+    .angleAccPedal = 0,
+    .angleBrakePedal = 50,
+    .requestCarStart = 1
+  };
+  
+  BSG bsg = {
+    .currentMode = BSG_INDLE,
+    .rpm = 4000,
+    .voltage = 48,
+    .current = 0
+  };
+  
+  Engine engine = {
+    .rpm = 4000
+  };
+  
+  Battery battery = {
+    .voltage = 30,
+    .current = 1
+  };
 
   const StateBSG bsgMode = selectBSGMode(vehicle, engine, bsg, battery);
   ASSERT_EQ(bsgMode, BSG_GENERATOR);
