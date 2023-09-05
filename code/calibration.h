5d1a6eb098c77ab21aeb9f6beb6ae341259698dc
Rafael <akafael@users.noreply.github.com>
2023-09-05 03:33:14 -0300

    Add calibration.h with all project calibration constants

---
+++ code/calibration.h
@@ -0,0 +1,11 @@
+#ifndef CALIBRATION_H
+
+const float BSGMaxVoltage = 55;
+
+const uint16_t EngineMaxRPM = 6000;
+const uint16_t EngineMinOperationRPM = 1000;
+
+const float BatteryMinVoltage = 10;
+const float BatteryMaxVoltage = 55;
+
+#endif 
