c976f35886ac224a321fab6ede43dedace0e4aa0
Rafael L <akafael@users.noreply.github.com>
2023-09-05 15:09:36 -0300

    Set BatteryMaxVoltage to 60
    Co-authored-by: pdsfilho <p.dantas@outlook.com>

--- code/calibration.h
+++ code/calibration.h
@@ -6,6 +6,6 @@ const uint16_t EngineMaxRPM = 10000;
 const uint16_t EngineMinOperationRPM = 1000;
 
 const float BatteryMinVoltage = 10;
-const float BatteryMaxVoltage = 55;
+const float BatteryMaxVoltage = 60;
 
 #endif 
