197d70478695c327524a9fed5ac7c7e344eab6da
Paulo <p.dantas@outlook.com>
2023-09-07 14:37:55 +0000

    Fixed extern const to be recognized to the CI

--- code/calibration.h
+++ code/calibration.h
@@ -1,14 +1,14 @@
 #ifndef CALIBRATION_H
 #include <stdint.h>
 
-const uint8_t BSGMaxVoltage;
+extern const uint8_t BSGMaxVoltage;
 
-const uint16_t EngineMaxRPM;
-const uint16_t EngineMinOperationRPM;
+extern const uint16_t EngineMaxRPM;
+extern const uint16_t EngineMinOperationRPM;
 
-const uint8_t BatteryMinVoltage;
-const uint8_t BatteryMinOperationVoltage;
-const uint8_t BatteryMaxVoltage;
-const uint8_t BatterySpecVoltage;
+extern const uint8_t BatteryMinVoltage;
+extern const uint8_t BatteryMinOperationVoltage;
+extern const uint8_t BatteryMaxVoltage;
+extern const uint8_t BatterySpecVoltage;
 
 #endif 
