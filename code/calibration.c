73b412775f399edc1c9d3c7376714b0443427b47
Paulo <p.dantas@outlook.com>
2023-09-07 02:01:54 +0000

    Included calibration.h for CI

--- code/calibration.c
+++ code/calibration.c
@@ -1,11 +1,12 @@
 #include <stdint.h>
+#include "calibration.h"
 
- uint8_t BSGMaxVoltage = 55;
+const uint8_t BSGMaxVoltage = 55;
 
- uint16_t EngineMaxRPM = 10000;
- uint16_t EngineMinOperationRPM = 1000;
+const uint16_t EngineMaxRPM = 10000;
+const uint16_t EngineMinOperationRPM = 1000;
 
- uint8_t BatteryMinVoltage = 5;
- uint8_t BatteryMinOperationVoltage = 7;
- uint8_t BatteryMaxVoltage = 60;
- uint8_t BatterySpecVoltage = 12;
+const uint8_t BatteryMinVoltage = 5;
+const uint8_t BatteryMinOperationVoltage = 7;
+const uint8_t BatteryMaxVoltage = 60;
+const uint8_t BatterySpecVoltage = 12;
