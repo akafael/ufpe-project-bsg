52ace48a5582b0cf3d104eb06e8c8917b0e47450
Paulo <p.dantas@outlook.com>
2023-09-07 01:13:17 +0000

    Changed calibration.c for CI

--- code/calibration.c
+++ code/calibration.c
@@ -1,11 +1,11 @@
 #include <stdint.h>
 
-const uint8_t BSGMaxVoltage = 55;
+ uint8_t BSGMaxVoltage = 55;
 
-const uint16_t EngineMaxRPM = 10000;
-const uint16_t EngineMinOperationRPM = 1000;
+ uint16_t EngineMaxRPM = 10000;
+ uint16_t EngineMinOperationRPM = 1000;
 
-const uint8_t BatteryMinVoltage = 5;
-const uint8_t BatteryMinOperationVoltage = 7;
-const uint8_t BatteryMaxVoltage = 60;
-const uint8_t BatterySpecVoltage = 12;
+ uint8_t BatteryMinVoltage = 5;
+ uint8_t BatteryMinOperationVoltage = 7;
+ uint8_t BatteryMaxVoltage = 60;
+ uint8_t BatterySpecVoltage = 12;
