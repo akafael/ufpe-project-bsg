17aed5629be9791fc6407ec62b9e7325fdba27f9
Paulo <p.dantas@outlook.com>
2023-09-07 00:22:06 +0000

    Implemented more tests for Battery

--- code/battery.c
+++ code/battery.c
@@ -9,5 +9,11 @@ StateBattery getBatteryState(Battery battery){
     if(battery.voltage < BatteryMinVoltage){
         return BATTERY_DEAD;
     }
-    return BATTERY_CHARGING;
+    if(battery.voltage == BatterySpecVoltage){
+        return BATTERY_OPERATIONAL;
+    }
+    if(battery.voltage < BatteryMinOperationVoltage){
+        return BATTERY_LOW;
+    }
+    return BATTERY_OPERATIONAL;
 }
--- code/test_battery.c
+++ code/test_battery.c
@@ -3,7 +3,6 @@
 
 UTEST_MAIN();
 
-
 UTEST(battery, batteryCharging){
   Battery battery = {
     .voltage = 25,
@@ -13,3 +12,33 @@ UTEST(battery, batteryCharging){
   const StateBattery batteryMode = getBatteryState(battery);
   ASSERT_EQ(batteryMode, BATTERY_CHARGING);
 }
+UTEST(battery, dead){
+  Battery battery = {
+    .voltage = 2,
+    .current = 1
+  };
+
+  const StateBattery batteryMode = getBatteryState(battery);
+  ASSERT_EQ(batteryMode, BATTERY_DEAD);
+}
+
+UTEST(battery, low){
+  Battery battery = {
+    .voltage = 5,
+    .current = 1
+  };
+
+  const StateBattery batteryMode = getBatteryState(battery);
+  ASSERT_EQ(batteryMode, BATTERY_LOW);
+}
+
+UTEST(battery, operacional){
+  Battery battery = {
+    .voltage = 8,
+    .current = 1
+  };
+
+  const StateBattery batteryMode = getBatteryState(battery);
+  ASSERT_EQ(batteryMode, BATTERY_OPERATIONAL);
+}
+  
