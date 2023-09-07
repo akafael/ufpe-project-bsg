1e0e34f5271a5ebaec1e81d2db22643dc69a27d2
SSDaniel <danielsarmento9@gmail.com>
2023-09-06 23:43:37 -0300

    adding to calibration file a correction proposed by Akafael

--- code/calibration.h
+++ code/calibration.h
@@ -1,11 +1,11 @@
 #ifndef CALIBRATION_H
 
-const float BSGMaxVoltage = 55;
+extern const float BSGMaxVoltage = 55;
 
-const uint16_t EngineMaxRPM = 10000;
-const uint16_t EngineMinOperationRPM = 1000;
+extern const uint16_t EngineMaxRPM = 10000;
+extern const uint16_t EngineMinOperationRPM = 1000;
 
-const float BatteryMinVoltage = 10;
-const float BatteryMaxVoltage = 60;
+extern const float BatteryMinVoltage = 10;
+extern const float BatteryMaxVoltage = 60;
 
 #endif 
