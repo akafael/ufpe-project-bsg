dbe2ed86e273c03c04c73f61b6b3c939ba5471ce
Rafael <akafael@users.noreply.github.com>
2023-09-07 01:57:03 -0300

    Fix ld error: multiple definition

---
+++ code/calibration.c
@@ -0,0 +1,11 @@
+#include <stdint.h>
+
+ uint8_t BSGMaxVoltage = 55;
+
+ uint16_t EngineMaxRPM = 10000;
+ uint16_t EngineMinOperationRPM = 1000;
+
+ uint8_t BatteryMinVoltage = 5;
+ uint8_t BatteryMinOperationVoltage = 7;
+ uint8_t BatteryMaxVoltage = 60;
+ uint8_t BatterySpecVoltage = 12;
--- code/calibration.h
+++ code/calibration.h
@@ -1,11 +1,14 @@
 #ifndef CALIBRATION_H
+#include <stdint.h>
 
-const float BSGMaxVoltage = 55;
+extern const uint8_t BSGMaxVoltage;
 
-const uint16_t EngineMaxRPM = 10000;
-const uint16_t EngineMinOperationRPM = 1000;
+extern const uint16_t EngineMaxRPM;
+extern const uint16_t EngineMinOperationRPM;
 
-const float BatteryMinVoltage = 10;
-const float BatteryMaxVoltage = 60;
+extern const uint8_t BatteryMinVoltage;
+extern const uint8_t BatteryMinOperationVoltage;
+extern const uint8_t BatteryMaxVoltage;
+extern const uint8_t BatterySpecVoltage;
 
 #endif 
