86c0d5a9073af32f96ec1199c77d11f947db1f2f
pjmbs <pedrobichara@gmail.com>
2023-09-09 11:52:42 -0300

    code to generate gifs from pictures generated in matlab

---
+++ code/gif-gen.py
@@ -0,0 +1,22 @@
+from PIL import Image
+import os
+
+files_directory = 'matlab_frames'
+
+files = os.listdir(files_directory)
+
+images = [img for img in files if img.endswith(".png")]
+
+images.sort(key=lambda x: int(x.split('_')[1].split('.')[0])) #sorts the images based on the number written in the image name
+
+#GIF creation
+
+frames = []
+for image in images:
+    path_image = os.path.join(files_directory, image)
+    frame = Image.open(path_image)
+    frames. append(frame)
+
+frames[0].save('gif.gif', save_all=True, append_images=frames[1:], duration=500, loop=0)
+
+
---
+++ matlab_test_github_action.m
@@ -0,0 +1,25 @@
+t = 0:1:50;
+y = t.^2;
+
+% Start the Figure
+figure;
+
+mkdir('matlab_frames');
+
+% Creates and saves images in a loop
+for i = 1:length(t)
+    plot(t(1:i), y(1:i));
+    title('y = t^2');
+    xlabel('time');
+    ylabel('response');
+    drawnow; % Updates the figure
+    frame = getframe(gcf); % Captures the frame
+    im = frame2im(frame); % Converts to image
+    filename = ['matlab_frames/frame_' num2str(i) '.png']; % Saves in the correct folder'
+    imwrite(im, filename, 'png');
+end
+
+
+
+
+
