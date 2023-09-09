794afff9a147db7cf546f5b7aeba3132e2f1381e
Rafael <akafael@users.noreply.github.com>
2023-09-09 00:46:38 -0300

    Move *.c and *.h to the code folder and update Makefile to include tests

--- code/vehicle.c
+++ code/vehicle.c
@@ -1,1 +1,20 @@
+#include "calibration.h"
 #include "vehicle.h"
+
+DriverIntention getDriverIntention(Vehicle vehicle){
+
+    if (vehicle.angleAccPedal < VehicleMinAccPedal && vehicle.angleBrakePedal < VehicleMinBrakePedal && vehicle.velocity < VehicleMinVelocity) {
+        return DRIVE_INTENTION_NOTHING; 
+    }
+    if(vehicle.angleAccPedal < VehicleMinAccPedal && vehicle.angleBrakePedal < VehicleMinBrakePedal){
+        return DRIVE_INTENTION_KEEP_SPEED;
+    }
+    if(vehicle.angleAccPedal > VehicleHighAccPedal && vehicle.angleBrakePedal < VehicleMinBrakePedal){
+        return DRIVE_INTENTION_INCREASE_SPEED;
+    }
+    if(vehicle.angleAccPedal < VehicleMinAccPedal && vehicle.angleBrakePedal > VehicleHighBrakePedal){
+        return DRIVE_INTENTION_REDUCE_SPEED;
+    }
+ 
+    return DRIVE_INTENTION_NOTHING;
+}
