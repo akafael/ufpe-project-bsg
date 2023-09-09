2bb0c099f91d86844cd8d8f51f240818499d12bd
Paulo <p.dantas@outlook.com>
2023-09-09 19:44:21 +0000

    Modified functions for CSV reading

--- code/car.c
+++ code/car.c
@@ -1,11 +1,36 @@
 #include "car.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdbool.h>
+
+#define MAXCHAR 1000
 
 
 //The token ";" is used to make separations between lines
+const char readCSV(){
+	FILE *fp;
+    char row[MAXCHAR];
+    char *token;
+
+    fp = fopen("requirements/Free_Test_Data_200KB_CSV-1","r");
+
+    while ( fgets(row, MAXCHAR, fp) != EOF )
+    {
+        fgets(row, MAXCHAR, fp);
+        printf("Row: %s", row);
+
+        token = strtok(row, ",");
+
+        while(token != NULL)
+        {
+            printf("Token: %s\n", token);
+            token = strtok(NULL, ",");
+        }
+
+    }
+    return NULL;
+}
+
 const char* getfield(char* line, int num)
 {
 	const char* tok;
@@ -19,12 +44,12 @@ const char* getfield(char* line, int num)
 	return NULL;
 }
 
-void readCSV(){
-    Car line[1024];
+void readField(){
+	char line[1024];
     while (fgets(line, 1024, stdin))
 	{
 		char* tmp = strdup(line);
-		printf("Car Data: %s\n", getfield(tmp, 1));
+		printf("Field 3 would be %s\n", getfield(tmp, 3));
 		// NOTE strtok clobbers tmp
 		free(tmp);
 	}
