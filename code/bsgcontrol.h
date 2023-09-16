eccbf9ceb224695b6b251a16ad35c23e45cc5ae2
Rafael <akafael@users.noreply.github.com>
2023-09-15 23:19:42 -0300

    Add documentation to bsgcontrol

--- code/bsgcontrol.h
+++ code/bsgcontrol.h
@@ -8,13 +8,13 @@
  * @brief Define the expected BSG operation mode considering the current measurement
  * from vehicle sensor data.
  * 
- * @param vehicle Vehicle Driver and Odometry information 
- * @param engine Engine sensor data (RPM)
- * @param bsg  BSG sensor data (RPM,Voltage,Current)
- * @param battery Battery sensor data (Voltage)
+ * @param[in] vehicle Vehicle Driver and Odometry information 
+ * @param[in] engine Engine sensor data (RPM)
+ * @param[in] bsg  BSG sensor data (RPM,Voltage,Current)
+ * @param[in] battery Battery sensor data (Voltage)
  * @return StateBSG 
  */
-StateBSG selectBSGMode(Vehicle vehicle, Engine engine, BSG bsg,
-                       Battery battery);
+StateBSG selectBSGMode(const Vehicle vehicle,const Engine engine,const BSG bsg,
+                       const Battery battery);
 
 #endif // BSG_CONTROL_H
