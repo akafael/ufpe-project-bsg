2f1b4708fbad63f98a62099e1698ff58c8f63e39
emannuellucas <lukas.lll@hotmail.com>
2023-09-16 17:16:31 -0300

    add test for calibration

--- code/calibration.c
+++ code/calibration.c
@@ -1,14 +1,15 @@
 #include <stdint.h>
 #include "calibration.h"
 
-const uint8_t BSGMaxVoltage = 55;
+const uint8_t BSGMaxVoltage = 50;
 
 const uint16_t EngineRPMNeutral = 1000;
 const uint16_t EngineRPMMaximum = 8000;
 
 
 const uint8_t BatteryMinVoltage = 5;
-const uint8_t BatteryMaxVoltage = 60;
+const uint8_t BatteryMinOperationVoltage = 7;
+const uint8_t BatteryMaxVoltage = 50;
 const uint8_t BatterySpecVoltage = 12;
 
 const int8_t BatteryMinOperationalCurrent = 1;
