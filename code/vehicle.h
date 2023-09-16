7767f180ea00b9d5c28b3982402fa45d6d1f958c
Rafael <akafael@users.noreply.github.com>
2023-09-15 23:17:02 -0300

    Add documentation to vehicle.c

--- code/vehicle.h
+++ code/vehicle.h
@@ -4,26 +4,34 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+/**
+ * @brief Vehicle odometer and driver input data from pedals
+ * 
+ */
 typedef struct {
-  uint16_t velocity;
-  uint16_t angleAccPedal;
-  uint16_t angleBrakePedal;
-  uint8_t requestCarStart;
+  uint16_t velocity;                   ///< [km/h] Vehicle velocity
+  uint16_t angleAccPedal;              ///< [%] Vehicle throttle pedal pressed percentage 
+  uint16_t angleBrakePedal;            ///< [%] Vehicle brake pedal pressed percentage
+  uint8_t requestCarStart;             ///< [bool] Vehicle signil from requesting engine start
 } Vehicle;
 
+/**
+ * @brief DriverIntention state representation considering Vehicle data
+ * 
+ */
 typedef enum {
-  DRIVE_INTENTION_NOTHING = 0,
-  DRIVE_INTENTION_REDUCE_SPEED = 1,
-  DRIVE_INTENTION_KEEP_SPEED = 2,
-  DRIVE_INTENTION_INCREASE_SPEED = 3,
+  DRIVE_INTENTION_NOTHING = 0,         ///< DriveIntention state for no input from driver
+  DRIVE_INTENTION_REDUCE_SPEED = 1,    ///< DriveIntention state for reducing vehicle speed intention
+  DRIVE_INTENTION_KEEP_SPEED = 2,      ///< DriveIntention state for keep vehicle speed intention
+  DRIVE_INTENTION_INCREASE_SPEED = 3,  ///< DriveIntention state for increase vehicle speed intention
 } DriverIntention;
 
 /**
  * @brief Translate the current input from vehicle odometry into a DriverIntention state
  * 
- * @param vehicle Vehicle Driver and Odometry data
+ * @param[in] vehicle Vehicle Driver and Odometry data
  * @return DriverIntention 
  */
-DriverIntention getDriverIntention(Vehicle vehicle);
+DriverIntention getDriverIntention(const Vehicle vehicle);
 
 #endif // VEHICLE_H
