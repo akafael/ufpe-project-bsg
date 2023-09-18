00fd149fd7dda399e38080291e38cc3be9b952f9
Lucas Emanuel <42306199+emannuellucas@users.noreply.github.com>
2023-09-18 15:28:12 -0300

    Update test_engine.c

--- code/test_engine.c
+++ code/test_engine.c
@@ -5,9 +5,9 @@ UTEST_MAIN()
 
 UTEST(engine, engineOff){
   Engine engine= {
-    .rpm = 0,
-    .velocity = 0,
-    .gear = 1,
+    .rpm = 25,
+    .velocity = 150,
+    .gear = 4,
   };
 
   const StateEngine test_engine = getEngineState(engine);
