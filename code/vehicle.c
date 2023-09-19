e1e203493ffc981309d85398d73b19e8369cff4b
SSDaniel <danielsarmento9@gmail.com>
2023-09-19 20:46:20 -0300

    All IF's have now been replaced by ELSE IF and the final ELSE has been added as a single return point.

--- code/vehicle.c
+++ code/vehicle.c
@@ -6,15 +6,16 @@ DriverIntention getDriverIntention(const Vehicle vehicle){
     if ( vehicle.angleAccPedal < VehicleMinAccPedal && vehicle.angleBrakePedal < VehicleMinBrakePedal && vehicle.velocity < VehicleMinVelocity) {
         return DRIVE_INTENTION_NOTHING; 
     }
-    if(vehicle.angleAccPedal < VehicleMinAccPedal && vehicle.angleBrakePedal < VehicleMinBrakePedal){
+    else if(vehicle.angleAccPedal < VehicleMinAccPedal && vehicle.angleBrakePedal < VehicleMinBrakePedal){
         return DRIVE_INTENTION_KEEP_SPEED;
     }
-    if(vehicle.angleAccPedal > VehicleMinAccPedal && vehicle.angleBrakePedal < VehicleMinBrakePedal){
+    else if(vehicle.angleAccPedal > VehicleMinAccPedal && vehicle.angleBrakePedal < VehicleMinBrakePedal){
         return DRIVE_INTENTION_INCREASE_SPEED;
     }
-    if(vehicle.angleAccPedal < VehicleMinAccPedal && vehicle.angleBrakePedal > VehicleMinBrakePedal){
+    else if(vehicle.angleAccPedal < VehicleMinAccPedal && vehicle.angleBrakePedal > VehicleMinBrakePedal){
         return DRIVE_INTENTION_REDUCE_SPEED;
     }
- 
+    else {
     return DRIVE_INTENTION_NOTHING;
+    }
 }
