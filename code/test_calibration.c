a79947ddfd4f2dae721b522376d8d3d72c2a2cdb
emannuellucas <lukas.lll@hotmail.com>
2023-09-20 23:14:37 -0300

    add test for calibration

--- code/test_calibration.c
+++ code/test_calibration.c
@@ -9,43 +9,40 @@
 UTEST_MAIN()
 
 UTEST(calibration, BatteryMaxVoltage) {
-    ASSERT_TRUE(BatteryMaxVoltage > 0 && BatteryMaxVoltage <= 50);
+    ASSERT_TRUE(BatteryMaxVoltage > 0);
+    ASSERT_TRUE(BatteryMaxVoltage <= 50);
 }
 
 UTEST(calibration, BSGMaxVoltage) {
-    ASSERT_TRUE(BSGMaxVoltage > 0 && BSGMaxVoltage <= 50);
+    ASSERT_TRUE(BSGMaxVoltage > 0);
+    ASSERT_TRUE(BSGMaxVoltage <= 50);
 }
 
 UTEST(calibration, BatteryMinVoltage ) {
-    ASSERT_TRUE(BatteryMinVoltage >= 0 && BatteryMinVoltage <= BatteryMinOperationVoltage );
-}
-
-UTEST(calibration, BatteryMinOperationVoltage ) {
-    ASSERT_TRUE(BatteryMinOperationVoltage > BatteryMinVoltage && BatteryMinOperationVoltage < BatterySpecVoltage );
+    ASSERT_TRUE(BatteryMinVoltage >= 0);
+    ASSERT_TRUE(BatteryMinVoltage <= BatterySpecVoltage);
 }
 
 UTEST(calibration, BatterySpecVoltage ) {
-    ASSERT_TRUE(BatterySpecVoltage > BatteryMinOperationVoltage && BatterySpecVoltage < BatteryMaxVoltage );
+    ASSERT_TRUE(BatterySpecVoltage > BatteryMinVoltage);
+    ASSERT_TRUE(BatterySpecVoltage < BatteryMaxVoltage);
 }
 
-UTEST(calibration, VehicleMinAccPedal  ) {
-    ASSERT_TRUE(VehicleMinAccPedal  >= 0 && VehicleMinAccPedal  < VehicleHighAccPedal  );
+UTEST(calibration, VehicleMinAccPedal) {
+    ASSERT_TRUE(VehicleMinAccPedal >= 0);
+    ASSERT_TRUE(VehicleMinAccPedal < VehicleHighAccPedal);
 }
 
-UTEST(calibration, VehicleMinBrakePedal ) {
-    ASSERT_TRUE(VehicleMinBrakePedal >= 0 && VehicleMinBrakePedal < VehicleHighBrakePedal );
-}
-
-
-UTEST(calibration, VehicleMinVelocity ) {
-    ASSERT_TRUE( VehicleMinVelocity  >= 0 && VehicleMinVelocity < 10 );
+UTEST(calibration, VehicleMinBrakePedal) {
+    ASSERT_TRUE(VehicleMinBrakePedal >= 0);
+    ASSERT_TRUE(VehicleMinBrakePedal < VehicleHighBrakePedal);
 }
 
 UTEST(calibration, EngineRPMNeutral) {
-    ASSERT_TRUE( EngineRPMNeutral >= 0 && EngineRPMNeutral < EngineRPMMaximum );
+    ASSERT_TRUE(EngineRPMNeutral >= 0 && EngineRPMNeutral < EngineRPMMaximum);
 }
 
 UTEST(calibration, EngineRPMMaximum) {
-    ASSERT_TRUE( EngineRPMMaximum >= 0 && EngineRPMMaximum <= 8000 );
+    ASSERT_TRUE(EngineRPMMaximum >= 0 && EngineRPMMaximum <= 8000);
 }
 
