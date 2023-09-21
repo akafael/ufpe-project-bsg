17f0e2e48a230e07129257e7939bebf2b5580863
SSDaniel <danielsarmento9@gmail.com>
2023-09-20 22:16:40 -0300

    MISRA   VIOLATION FIXES

--- code/vehicle.c
+++ code/vehicle.c
@@ -1,21 +1,22 @@
 #include "calibration.h"
 #include "vehicle.h"
 
-DriverIntention getDriverIntention(const Vehicle vehicle){
+DriverIntention driverIntention = DRIVER_INTENTION_NOTHING;
 
-    if ( vehicle.angleAccPedal < VehicleMinAccPedal && vehicle.angleBrakePedal < VehicleMinBrakePedal && vehicle.velocity < VehicleMinVelocity) {
-        return DRIVE_INTENTION_NOTHING; 
+    if( vehicle.angleBrakePedal > VehicleMinBrakePedal )
+    {
+        driverIntention = DRIVE_INTENTION_KEEP_SPEED;
     }
-    else if(vehicle.angleAccPedal < VehicleMinAccPedal && vehicle.angleBrakePedal < VehicleMinBrakePedal){
-        return DRIVE_INTENTION_KEEP_SPEED;
+    else
+    {
+        if( vehicle.angleAccPedal > VehicleMinAccPedal )
+        {
+            driverIntention = DRIVE_INTENTION_INCREASE_SPEED;
+        }
+        else
+        {
+            driverIntention = DRIVE_INTENTION_NOTHING; 
+        }
     }
-    else if(vehicle.angleAccPedal > VehicleMinAccPedal && vehicle.angleBrakePedal < VehicleMinBrakePedal){
-        return DRIVE_INTENTION_INCREASE_SPEED;
-    }
-    else if(vehicle.angleAccPedal < VehicleMinAccPedal && vehicle.angleBrakePedal > VehicleMinBrakePedal){
-        return DRIVE_INTENTION_REDUCE_SPEED;
-    }
-    else {
-    return DRIVE_INTENTION_NOTHING;
-    }
-}
+    return driverIntention;
+    
