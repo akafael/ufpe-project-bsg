f2a4e73c5f89c74693c052d49d8958aea768368b
Rafael <akafael@users.noreply.github.com>
2023-09-21 23:30:50 -0300

    Add two test cases to test_engine

--- code/engine.c
+++ code/engine.c
@@ -1,23 +1,17 @@
 #include "engine.h"
 #include "calibration.h"
 
+StateEngine getEngineState(Engine engine) {
+  StateEngine stateEngine = ENGINE_OFF;
 
-
-StateEngine getEngineState(Engine engine){
-    StateEngine stateEngine = ENGINE_OFF;
-
-    if( engine.rpm == 0 ){
-        stateEngine = ENGINE_OFF;
+  if (engine.rpm == 0) {
+    stateEngine = ENGINE_OFF;
+  } else {
+    if (engine.rpm >= EngineRPMMaximum) {
+      stateEngine = ENGINE_RPM_MAXIMUM;
+    } else {
+      stateEngine = ENGINE_WORKING;
     }
-    else{
-        if( engine.rpm >= EngineRPMMaximum ){
-        stateEngine = ENGINE_RPM_MAXIMUM;
-        }
-        else 
-        {
-        stateEngine = ENGINE_WORKING;
-        }
-    }
-    return stateEngine;
-    
+  }
+  return stateEngine;
 }
